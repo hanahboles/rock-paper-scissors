d99b343a33e2f2148f821dd857a8b40b8b75a707
Hanah Boles <58400558+hanahboles@users.noreply.github.com>
2023-09-10 12:35:12 -0700

    add buttons for player selection and to trigger a new round of the game

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-function game() {
-    let numRounds = 5
-    let score = 0
-    let computerScore = 0
+// function game() {
+//     let numRounds = 5
+//     let score = 0
+//     let computerScore = 0
 
-    for (i = 0; i < numRounds; i++) {
-        let playerSelection = getPlayerChoice()
-        let computerSelection = getComputerChoice()
-        let game = playRound(playerSelection, computerSelection)
-        game.win ? score++ : computerScore++
-        console.log('your score', score)
-        console.log('computer score', computerScore)
-        alert(game.message)
-        // if game is a tie, need to add another round
-        if (game.win === null) {
-            numRounds++
-        }
-    }
+//     for (i = 0; i < numRounds; i++) {
+//         let playerSelection = getPlayerChoice()
+//         let computerSelection = getComputerChoice()
+//         let game = playRound(playerSelection, computerSelection)
+//         game.win ? score++ : computerScore++
+//         console.log('your score', score)
+//         console.log('computer score', computerScore)
+//         alert(game.message)
+//         // if game is a tie, need to add another round
+//         if (game.win === null) {
+//             numRounds++
+//         }
+//     }
 
-    if (score > computerScore) {
-        alert('You win the game!')
-    } else {
-        alert('You lose the game')
+//     if (score > computerScore) {
+//         alert('You win the game!')
+//     } else {
+//         alert('You lose the game')
+//     }
+// }
+
+function onButtonClick(e) {
+    const playerSelection = e.target.dataset.key
+    let computerSelection = getComputerChoice()
+    let game = playRound(playerSelection, computerSelection)
+    alert(game.message)
+    if (game.win === null) return
+    game.win ? score++ : computerScore++
+    playerScoreText.innerText = `Your Score: ${score}`
+    computerScoreText.innerText = `Computer Score: ${computerScore}`
+    if (score === 5 || computerScore === 5) {
+        const msg = score > computerScore ? 'You win the game' : 'You lose the game'
+        alert(msg)
     }
 }
 
 function playRound(playerSelection, computerSelection) {
+
     if (playerSelection === computerSelection) {
         return {win: null, message: "Tie!"}
     }
@@ -47,15 +63,12 @@ function getComputerChoice() {
     }
 }
 
-function getPlayerChoice() {
-    const playerSelection = prompt("Rock, paper, or scissors?").trim().toLowerCase()
+const buttons = document.getElementsByTagName('button')
+const playerScoreText = document.querySelector('.user-score')
+const computerScoreText = document.querySelector('.computer-score')
+Array.from(buttons).forEach((btn) => btn.addEventListener('click', onButtonClick))
+let score = 0
+let computerScore = 0
 
-    if (playerSelection !== 'rock' && playerSelection !== 'scissors' && playerSelection !== 'paper') {
-        alert('Please type a valid choice')
-        return getPlayerChoice()
-    } else {
-        return playerSelection
-    }
-}
 
-game()
+// game()
